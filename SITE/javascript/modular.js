7616e5071d8a16c96e42e1cb85f54b9784f698b5
caiodntas <caiodantasprofissional@outlook.com>
2021-05-25 11:21:18 -0300

    ada

--- SITE/javascript/modular.js
+++ SITE/javascript/modular.js
@@ -1,60 +1,63 @@
 let Counter = 0;
 
-        /* FUNÇÃO QUE FAZ COM QUE O ITEM SELECIONADO RECEBA UM FOCO */
-        function select(){
-        };
+/* FUNÇÃO QUE FAZ COM QUE O ITEM SELECIONADO RECEBA UM FOCO */
+function select(){
+};
 
-        function unselect(){
-        };
+function unselect(){
+};
 
-        /* FUNCÃO QUE FAZ COM QUE QUANDO O BOTÃO DE ESCREVA FOR APERTADO ELE CRIE UMA DIV COM UM PARÁGRAFO
-        EDITÁVEL COM O FOCO NELE E QUANDO PASSE O MOUSE EM CIMA ELE RECEBA UM FOCO */
-        function textcreate (){
-            /* Criando e declarando as variáveis como uma tag */
-            var CreateDiv = document.createElement("div");
-            var Paragraph = document.createElement("p");
-            Counter++;
+/* FUNCÃO QUE FAZ COM QUE QUANDO O BOTÃO DE ESCREVA FOR APERTADO ELE CRIE UMA DIV COM UM PARÁGRAFO
+EDITÁVEL COM O FOCO NELE E QUANDO PASSE O MOUSE EM CIMA ELE RECEBA UM FOCO */
+    function textcreate (){
+        /* Criando e declarando as variáveis como uma tag */
+        var CreateDiv = document.createElement("div");
+        var Paragraph = document.createElement("p");
+        Counter++;
 
-            /* Dando uma classe para as variáveis */
-            CreateDiv.classList.add('textmodule');
-            Paragraph.classList.add('paragraph','selected');
+        /* Dando uma classe para as variáveis */
+        CreateDiv.classList.add('textmodule');
+        Paragraph.classList.add('paragraph','selected');
 
-            /* Torna a Div e tudo que está em seu interior arrastável */
-            CreateDiv.setAttribute('draggable', true);
-            CreateDiv.setAttribute('onmouseover', "select()");
-            CreateDiv.setAttribute('onmouseout', "unselect()");
-            CreateDiv.setAttribute('id', 'textdiv' + Counter);
-            Paragraph.setAttribute('contentEditable', true);
-            Paragraph.setAttribute('placeholder', "Digite aqui!");
+        /* Torna a Div e tudo que está em seu interior arrastável */
+        CreateDiv.setAttribute('draggable', true);
+        CreateDiv.setAttribute('onmouseover', "select()");
+        CreateDiv.setAttribute('onmouseout', "unselect()");
+        CreateDiv.setAttribute('id', 'textdiv' + Counter);
+        Paragraph.setAttribute('contentEditable', true);
+        Paragraph.setAttribute('placeholder', "Digite aqui!");
 
-            /* Coloca as Variáveis ao código quando o botão é apertado. */
-            document.getElementById('board').appendChild(CreateDiv);
-            document.getElementById('textdiv' + Counter).appendChild(Paragraph).focus();
-        };
+        /* Coloca as Variáveis ao código quando o botão é apertado. */
+        document.getElementById('board').appendChild(CreateDiv);
+        document.getElementById('textdiv' + Counter).appendChild(Paragraph).focus();
+    };
 
-        /* FUNCÃO QUE FAZ COM QUE QUANDO O BOTÃO DE MÍDIA FOR APERTADO ELE CRIE UMA DIV ONDE PODERÁ SER INSERI
-        DA UMA MÍDIA COM O FOCO NELE, E QUANDO PASSE O MOUSE EM CIMA ELE RECEBA UM FOCO */
-        function midiacreate (){
-            /* FAZ COM QUE O BOTÃO MÍDIA SEJA UM INPUT FILE */
-            const realfile = document.getElementById("real-midiabutton");
-            const custombutton = document.getElementById("midia");
-            realfile.click();
+    /* FUNCÃO QUE FAZ COM QUE QUANDO O BOTÃO DE MÍDIA FOR APERTADO ELE CRIE UMA DIV ONDE PODERÁ SER INSERI
+    DA UMA MÍDIA COM O FOCO NELE, E QUANDO PASSE O MOUSE EM CIMA ELE RECEBA UM FOCO */
+    function midiacreate (){
+        /* Adiciona um ao contador, para que os elementos sejam diferentes. */
+        Counter++;
+        /* FAZ COM QUE O BOTÃO MÍDIA SEJA UM INPUT FILE */
+        document.getElementById("real-midiabutton").click();
+        /* ARMAZENA A IMAGEM NO LOCAL STORAGE */
+        document.querySelector("#real-midiabutton").addEventListener("change", function(){
+            const reader = new FileReader();
 
-            /* SALVANDO O ARQUIVO DO USUÁRIO */
-            let img = loadImage('');
+            reader.addEventListener("load", () =>{
+                localStorage.setItem("recent-image" + Counter, reader.result);
+            })
 
-            var CreateDiv = document.createElement("div");
-            Counter++;
+            reader.readAsDataURL(this.files[0]);
+            });
 
-            CreateDiv.classList.add('midiamodule');
+        /* Torna a Div e tudo que está em seu interior arrastável */
+        var CreateDiv = document.createElement("div");
+        CreateDiv.setAttribute('draggable', true);
+        CreateDiv.setAttribute('onmouseover', "select()");
+        CreateDiv.setAttribute('onmouseout', "unselect()");
+        CreateDiv.setAttribute('id', 'midiadiv' + Counter);
 
-            /* Torna a Div e tudo que está em seu interior arrastável */
-            CreateDiv.setAttribute('draggable', true);
-            CreateDiv.setAttribute('onmouseover', "select()");
-            CreateDiv.setAttribute('onmouseout', "unselect()");
-            CreateDiv.setAttribute('id', 'midiadiv' + Counter);
-
-            /* Coloca as Variáveis ao código quando o botão é apertado. */
-            document.getElementById('board').appendChild(CreateDiv);
+        /* Coloca as Variáveis ao código quando o botão é apertado. */
+        document.getElementById('board').appendChild(CreateDiv);
 
         };
