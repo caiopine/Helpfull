d88bc4758e979e6f0d4660c05fd651a33496c963
caiodntas <caiodantasprofissional@outlook.com>
2021-05-25 15:16:30 -0300

    tentativas de tornar o botão imagem funcional

--- SITE/javascript/modular.js
+++ SITE/javascript/modular.js
@@ -19,7 +19,7 @@ EDITÁVEL COM O FOCO NELE E QUANDO PASSE O MOUSE EM CIMA ELE RECEBA UM FOCO */
         CreateDiv.classList.add('textmodule');
         Paragraph.classList.add('paragraph','selected');
 
-        /* Torna a Div e tudo que está em seu interior arrastável */
+        /* Torna o Módulo arrastável, selecionável e dá um id único. */
         CreateDiv.setAttribute('draggable', true);
         CreateDiv.setAttribute('onmouseover', "select()");
         CreateDiv.setAttribute('onmouseout', "unselect()");
@@ -32,32 +32,31 @@ EDITÁVEL COM O FOCO NELE E QUANDO PASSE O MOUSE EM CIMA ELE RECEBA UM FOCO */
         document.getElementById('textdiv' + Counter).appendChild(Paragraph).focus();
     };
 
+    /* FAZ COM QUE O BOTÃO MÍDIA SEJA UM INPUT FILE */
+    function execfilefield(){
+        document.getElementById("botaomidia").click();
+    }
     /* FUNCÃO QUE FAZ COM QUE QUANDO O BOTÃO DE MÍDIA FOR APERTADO ELE CRIE UMA DIV ONDE PODERÁ SER INSERI
     DA UMA MÍDIA COM O FOCO NELE, E QUANDO PASSE O MOUSE EM CIMA ELE RECEBA UM FOCO */
-    function midiacreate (){
-        /* Adiciona um ao contador, para que os elementos sejam diferentes. */
-        Counter++;
-        /* FAZ COM QUE O BOTÃO MÍDIA SEJA UM INPUT FILE */
-        document.getElementById("real-midiabutton").click();
-        /* ARMAZENA A IMAGEM NO LOCAL STORAGE */
-        document.querySelector("#real-midiabutton").addEventListener("change", function(){
-            const reader = new FileReader();
-
-            reader.addEventListener("load", () =>{
-                localStorage.setItem("recent-image" + Counter, reader.result);
-            })
-
-            reader.readAsDataURL(this.files[0]);
-            });
-
+    function midiacreate() {
+        image = document.getElementById("botaomidia").value;
+        
+        
         /* Torna a Div e tudo que está em seu interior arrastável */
         var CreateDiv = document.createElement("div");
+        var Media = document.createElement("img");
+
         CreateDiv.setAttribute('draggable', true);
         CreateDiv.setAttribute('onmouseover', "select()");
         CreateDiv.setAttribute('onmouseout', "unselect()");
         CreateDiv.setAttribute('id', 'midiadiv' + Counter);
+        Media.setAttribute('src', ""),('alt', "noimage");
+        Media.setAttribute('id', 'media' + Counter);
 
         /* Coloca as Variáveis ao código quando o botão é apertado. */
         document.getElementById('board').appendChild(CreateDiv);
+        document.getElementById('midiadiv' + Counter).appendChild(Media);
 
+        /* Adiciona um ao contador, para que os elementos sejam diferentes. */
+        Counter++;
         };
