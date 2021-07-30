bf13dbaacfe3085d070174a96ed2709e3875dfee
Caio Dantas <caiodantasprofissional@outlook.com>
2021-07-30 20:54:36 -0300

    DOZE HORAAAAAAS

--- DO INÍCIO/javascript/ColorScheme.js
+++ DO INÍCIO/javascript/ColorScheme.js
@@ -1,51 +1,49 @@
-window.onload = function (){
-    /* DECLARAÇÃO DAS VARIÁVEIS */
-    const Body = document.querySelector('body');
-    const ColorMode = document.getElementById('ColorModeButton');
-    const Logo = document.querySelector('img');
+/* DECLARAÇÃO DAS VARIÁVEIS */
+const Body = document.querySelector('body');
+const ColorMode = document.getElementById('ColorModeButton');
+const Logo = document.querySelector('img');
+/* 0 = Dark Mode; 1 = Light Mode; */
+var ColorModeSelection = 1;
 
-    /* 0 = Dark Mode; 1 = Light Mode; */
-    var ColorModeSelection = 1;
+ColorMode.onclick = ColorScheme;
 
-    ColorMode.onclick = ColorScheme;
+function ColorScheme(){
 
-    function ColorScheme(){
-        if (ColorModeSelection == 1){
-            /* MUDANÇA PARA PODER APERTAR O BOTÃO NOVAMENTE */
-            ColorModeSelection = 0;
+    if (ColorModeSelection == 1){
+        /* MUDANÇA PARA PODER APERTAR O BOTÃO NOVAMENTE */
+        ColorModeSelection = 0;
 
-            /* TROCA DAS CORES */
-            Body.style.setProperty('--background-color', '#333333');
-            Body.style.setProperty('--box-shadow', 'black');
-            Body.style.setProperty('--primary-text-color', 'white');
-            Body.style.setProperty('--primary-icons-color', 'white');
-            Body.style.setProperty('--hover-icons-color', 'gray');
-            Body.style.setProperty('--hover-language-color', '#505050');
-            Body.style.setProperty('--placeholder-color', '#dddddd');
-            /* INVERTE A COR DO LOGO */
-            Logo.src="../files/logo/WhiteLogo.png";
-            /* COLOCA UMA TRANSIÇÃO */
-            Body.style.setProperty('transition', '0.5s');
-            Body.style.setProperty('delay', '2s');
-        }
+        /* TROCA DAS CORES */
+        Body.style.setProperty('--background-color', '#333333');
+        Body.style.setProperty('--box-shadow', 'black');
+        Body.style.setProperty('--primary-text-color', 'white');
+        Body.style.setProperty('--primary-icons-color', 'white');
+        Body.style.setProperty('--hover-icons-color', 'gray');
+        Body.style.setProperty('--hover-language-color', '#505050');
+        Body.style.setProperty('--placeholder-color', '#dddddd');
+        /* INVERTE A COR DO LOGO */
+        Logo.src="../files/logo/WhiteLogo.png";
+        /* COLOCA UMA TRANSIÇÃO */
+        Body.style.setProperty('transition', '0.5s');
+        Body.style.setProperty('delay', '2s');
+    }
 
-        else {
-            /* MUDANÇA PARA PODER APERTAR O BOTÃO NOVAMENTE */
-            ColorModeSelection = 1;
+    else {
+        /* MUDANÇA PARA PODER APERTAR O BOTÃO NOVAMENTE */
+        ColorModeSelection = 1;
 
-            /* TROCA DAS CORES */
-            Body.style.setProperty('--background-color', 'rgb(245, 245, 245)');
-            Body.style.setProperty('--box-shadow', 'rgb(200, 200, 200)');
-            Body.style.setProperty('--primary-text-color', 'black');
-            Body.style.setProperty('--primary-icons-color', 'gray');
-            Body.style.setProperty('--hover-icons-color', '#333333');
-            Body.style.setProperty('--hover-language-color', 'rgb(210, 210, 210)');
-            Body.style.setProperty('--placeholder-color', 'gray');
-            /* INVERTE A COR DO LOGO */
-            Logo.src="../files/logo/BlackLogo.png";
-            /* COLOCA UMA TRANSIÇÃO */
-            Body.style.setProperty('transition', '0.5s');
-            Body.style.setProperty('delay', '2s');
-        }
+        /* TROCA DAS CORES */
+        Body.style.setProperty('--background-color', 'rgb(245, 245, 245)');
+        Body.style.setProperty('--box-shadow', 'rgb(200, 200, 200)');
+        Body.style.setProperty('--primary-text-color', 'black');
+        Body.style.setProperty('--primary-icons-color', 'gray');
+        Body.style.setProperty('--hover-icons-color', '#333333');
+        Body.style.setProperty('--hover-language-color', 'rgb(210, 210, 210)');
+        Body.style.setProperty('--placeholder-color', 'gray');
+        /* INVERTE A COR DO LOGO */
+        Logo.src="../files/logo/BlackLogo.png";
+        /* COLOCA UMA TRANSIÇÃO */
+        Body.style.setProperty('transition', '0.5s');
+        Body.style.setProperty('delay', '2s');
     }
 }
--- DO INÍCIO/javascript/Translate.js
+++ DO INÍCIO/javascript/Translate.js
@@ -1,12 +1,10 @@
-window.onload = function (){
+/* ABRIR A LISTA DE IDIOMAS */
+    /* VARIÁVEIS */
     const LanguageListButton = document.getElementById('LanguageListButton');
     const LanguageList = document.getElementById('LanguageList');
 
     LanguageListButton.onclick = OpenLangList;
-
     function OpenLangList() {
-        
-
         if (getComputedStyle(LanguageList).getPropertyValue('visibility') == 'hidden') {
             LanguageList.style.setProperty('visibility', 'visible');
         }
@@ -15,5 +13,63 @@ window.onload = function (){
             LanguageList.style.setProperty('visibility', 'hidden');
         }
     }
-    
-}
+
+/* TROCAR O IDIOMA */
+    /* OPÇÕES DE LÍNGUAS */
+    const PTBR = document.getElementById('PT-BR');
+    const ENUS = document.getElementById('EN-US');
+    const ES = document.getElementById('ES');
+    /* VARIÁVEIS */
+        /* LEFT-HEADER */
+        const WelcomeTitle = document.getElementById('WelcomeTitle');
+        const PresentationText = document.getElementById('PresentationText');
+        const LearnMoreButton = document.getElementById('LearnMoreButton');
+        /* RIGHT-HEADER */
+        const LoginFieldInputs = document.querySelectorAll('input');
+        const LoginButton = document.getElementById('LoginButton');
+        const ForgotText = document.getElementById('ForgetText');
+        const RegisterButton = document.getElementById('RegisterButton');
+        const BrowseAnonymously = document.getElementById('BrowseAnonymously');
+
+    /* AÇÕES AO CLICAR NO BOTÃO */
+        /* PT-BR */
+        PTBR.onclick = PTBRchange;
+        function PTBRchange() {
+            WelcomeTitle.innerText = 'Bem Vindo';
+            PresentationText.innerHTML = "A Helpfull é uma plataforma com <br> a missão de mudar o mundo, e <br> você é o protagonista nesta jornada.";
+            LearnMoreButton.innerText = "saiba como";
+            LoginFieldInputs[0].setAttribute('Placeholder', 'Email ou usuário');
+            LoginFieldInputs[1].setAttribute('Placeholder', 'Senha');
+            LoginButton.innerText = 'Entrar';
+            ForgotText.innerText = 'Esqueceu algo?';
+            RegisterButton.innerText = 'Criar nova conta';
+            BrowseAnonymously.innerText = 'Navegar Anonimamente';
+        }
+
+        /* EN-US */
+        ENUS.onclick = ENUSchange;
+        function ENUSchange() {
+            WelcomeTitle.innerHTML = 'Welcome';
+            PresentationText.innerHTML = "Helpfull is an plataform with the <br> mission of change the world <br> you are the protagonist in this journey.";
+            LearnMoreButton.innerText = "learn how";
+            LoginFieldInputs[0].setAttribute('Placeholder', 'Email or user');
+            LoginFieldInputs[1].setAttribute('Placeholder', 'Password');
+            LoginButton.innerText = 'Login';
+            ForgotText.innerText = 'Forgot something?';
+            RegisterButton.innerText = 'Create a new account';
+            BrowseAnonymously.innerText = 'Browse Anonymously';
+        }
+
+        /* ES */
+        ES.onclick = ESchange;
+        function ESchange() {
+            WelcomeTitle.innerHTML = 'Bienvenido';
+            PresentationText.innerHTML = "Helpfull es una plataforma con <br> la misión de cambiar el mundo, y <br> eres el protagonista de este viaje.";
+            LearnMoreButton.innerText = "sepa cómo";
+            LoginFieldInputs[0].setAttribute('Placeholder', 'Correo electrónico o usuario');
+            LoginFieldInputs[1].setAttribute('Placeholder', 'Contraseña');
+            LoginButton.innerText = 'Iniciar sesión';
+            ForgotText.innerText = '¿Olvidaste algo?';
+            RegisterButton.innerText = 'Crear una nueva cuenta';
+            BrowseAnonymously.innerText = 'Navegar de forma anónima';
+        }
