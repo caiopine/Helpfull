5b99f899ee753934ad5912c97c0f149f8fec80a1
Caio Dantas <caiodantasprofissional@outlook.com>
2021-09-27 08:09:48 -0300

    a

--- HELPFULL/html/php/login.php
+++ HELPFULL/html/php/login.php
@@ -5,6 +5,25 @@
     $usuario = $_POST["usuario"];
     $senha = $_POST["senha"];
 
-    $sql = mysql_query("select * from tb_login where (USUARIO="$usuario" or EMAIL="")
+    $sql = mysql_query("select * from tb_login where (USUARIO="$usuario" or EMAIL="$email" and senha="$senha");
 
-?>
+    if (mysql_num_rows($sql) > 0) {
+
+        echo "<script language=javascript>alert('Conta logada com sucesso!');</script>";
+
+        echo "<center>";
+        echo "<hr>";
+        echo "Conta logada com sucesso!";
+        echo "<hr>";
+        echo "<br>";
+    }
+
+    else {
+        echo "<center>";
+        echo "<hr>";
+        echo "Usuário ou Senha inválido!";
+        echo "<hr>";
+        echo "<br>";
+        echo "<a href=\"login.php\">Tentar novamente</a>
+    }
+?> 
