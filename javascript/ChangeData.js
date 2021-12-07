d73f715ce6c111d0ef4c20ad54b8a4482f81dc10
Caio Dantas <caiodantasprofissional@outlook.com>
2021-12-06 22:46:40 -0300

    a

--- javascript/ChangeData.js
+++ javascript/ChangeData.js
@@ -5,21 +5,19 @@ if (Logged == 1) {
     /* TROCA DO NOME DE PERFIL */
     StoredName = localStorage.getItem('Username');
     UserNameReceptor = document.querySelectorAll(".UserName");
-    UserNameReceptor[0].innerText = StoredName;
-    UserNameReceptor[1].innerText = StoredName;
+    UserNameReceptor[0],[1].innerText = StoredName;
+
+    /* TROCA DA FOTO DE PERFIL */
+    UserPicture = document.querySelectorAll('.UserPicture');
+    UserPicture[0],[1].setAttribute('src', "../files/database/projects/" + StoredName + "/profilepicture.jpg")
 
     /* TROCA DO REDIRECIONAMENTO DO PERFIL */
-    if (InUserProfile == 1) {
-        UsersContainers = document.querySelectorAll(".UsersContainers");
-        UsersContainers[0].setAttribute('href', "../files/database/projects/" + StoredName + "/" + StoredName + ".html");
-        UsersContainers[1].setAttribute('href', "../files/database/projects/" + StoredName + "/" + StoredName + ".html");
-    }
-
-    else {
-        
-    }
+    UsersContainers = document.querySelectorAll(".UsersContainers");
+    console.log(UsersContainers);
+    UsersContainers[0],[1].setAttribute('href', "../files/database/projects/" + StoredName + "/" + StoredName + ".html");
 }
 
 else {
+    localStorage.setItem('Username', 'Usuário');
 
 }
