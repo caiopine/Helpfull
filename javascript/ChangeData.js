27c93a15fe9df95fb93ec32d14b9fec78930353b
Caio Dantas <caiodantasprofissional@outlook.com>
2021-12-07 08:46:43 -0300

    a

--- javascript/ChangeData.js
+++ javascript/ChangeData.js
@@ -1,23 +1,27 @@
 var Logged = localStorage.getItem('Logged');
 var StoredName; var UserNameReceptor; var InUserProfile;
 
-if (Logged == 1) {
-    /* TROCA DO NOME DE PERFIL */
-    StoredName = localStorage.getItem('Username');
-    UserNameReceptor = document.querySelectorAll(".UserName");
-    UserNameReceptor[0],[1].innerText = StoredName;
+function ChangeData() {
+    if (Logged == 1) {
+        /* TROCA DO NOME DE PERFIL */
+        StoredName = localStorage.getItem('Username');
+        UserNameReceptor = document.querySelectorAll(".UserName");
+        UserNameReceptor[0].innerText = StoredName;
+        UserNameReceptor[1].innerText = StoredName;
 
-    /* TROCA DA FOTO DE PERFIL */
-    UserPicture = document.querySelectorAll('.UserPicture');
-    UserPicture[0],[1].setAttribute('src', "../files/database/projects/" + StoredName + "/profilepicture.jpg")
+        /* TROCA DA FOTO DE PERFIL */
+        UserPicture = document.querySelectorAll('.UserPicture');
+        UserPicture[0].setAttribute('src', "../files/database/projects/" + StoredName + "/profilepicture.jpg");
+        UserPicture[1].setAttribute('src', "../files/database/projects/" + StoredName + "/profilepicture.jpg")
 
-    /* TROCA DO REDIRECIONAMENTO DO PERFIL */
-    UsersContainers = document.querySelectorAll(".UsersContainers");
-    console.log(UsersContainers);
-    UsersContainers[0],[1].setAttribute('href', "../files/database/projects/" + StoredName + "/" + StoredName + ".html");
-}
+        /* TROCA DO REDIRECIONAMENTO DO PERFIL */
+        UsersContainers = document.querySelectorAll(".UsersContainers");
+        console.log(UsersContainers);
+        UsersContainers[0].setAttribute('href', "../files/database/projects/" + StoredName + "/" + StoredName + ".html");
+        UsersContainers[1].setAttribute('href', "../files/database/projects/" + StoredName + "/" + StoredName + ".html");
+    }
 
-else {
-    localStorage.setItem('Username', 'Usuário');
-
+    else {
+        localStorage.setItem('Username', 'Usuário');
+    }
 }
