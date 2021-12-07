b8935c80dd1830ad099235f3bafba9e953ffe6bd
Caio Dantas <caiodantasprofissional@outlook.com>
2021-12-07 14:36:54 -0300

    a

--- javascript/ChangeData.js
+++ javascript/ChangeData.js
@@ -1,5 +1,7 @@
 var Logged = localStorage.getItem('Logged');
-var StoredName; var UserNameReceptor; var InUserProfile;
+var StoredName; var UserNameReceptor;
+
+var actualhref = window.location.href.split("/");
 
 if (Logged == 1) {
     /* TROCA DO NOME DE PERFIL */
@@ -10,8 +12,16 @@ if (Logged == 1) {
 
     /* TROCA DA FOTO DE PERFIL */
     UserPicture = document.querySelectorAll('.UserPicture');
-    UserPicture[0].setAttribute('src', "../../../Helpfull/files/database/projects/" + StoredName + "/profilepicture.jpg");
-    UserPicture[1].setAttribute('src', "../../../Helpfull/files/database/projects/" + StoredName + "/profilepicture.jpg")
+
+    if (actualhref[7] == "Helpfull.html" || actualhref[7] == "timeline.html") {
+        UserPicture[0].setAttribute('src', "../files/database/projects/" + StoredName + "/profilepicture.jpg");
+        UserPicture[1].setAttribute('src', "../files/database/projects/" + StoredName + "/profilepicture.jpg");
+    }
+
+    else {  
+        UserPicture[0].setAttribute('src', "profilepicture.jpg");
+        UserPicture[1].setAttribute('src', "profilepicture.jpg");
+    }
 
     /* TROCA DO REDIRECIONAMENTO DO PERFIL */
     UsersContainers = document.querySelectorAll(".UsersContainers");
